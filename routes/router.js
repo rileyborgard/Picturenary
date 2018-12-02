3936cc8ecde2e028ec42ccd2d8f2e9546b6f25bf
Riley <rileyborgard@gmail.com>
2018-12-01 20:31:40 -0500

    Created registration page

--- routes/router.js
+++ routes/router.js
@@ -7,5 +7,8 @@ router.get('/', function(req, res) {
 router.get('/login', function(req, res) {
     res.render('login');
 });
+router.get('/register', function(req, res) {
+    res.render('register');
+});
 
 module.exports = router;
